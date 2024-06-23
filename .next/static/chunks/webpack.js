782dc30efb8b4f712fe13a42870e54e79bb3fbce
suryasardar <suryaprakashchary515@gmail.com>
2024-06-23 09:00:10 +0530

    done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7d78f482b5f005a6"; }
+/******/ 		__webpack_require__.h = function() { return "59f0da1dc8f22451"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/compoents/Workspace/Playground/Playground.tsx
+++ src/compoents/Workspace/Playground/Playground.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import PreferenceNav from "../Preference/PreferenceNav";
+import Split from "react-split";
+import CodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { javascript } from "@codemirror/lang-javascript";
+type PlaygroundProps = {};
 
-type PlaygroundProps = {
-    
+const Playground: React.FC<PlaygroundProps> = () => {
+  return (
+    <div className="flex flex-col bg-dark-layer-1 relative">
+      <PreferenceNav />
+      <Split
+        className="h-[calc(100vh-94px)]"
+        direction="vertical"
+        sizes={[60, 40]}
+        minSize={60}
+      >
+        <div className="w-full overflow-auto">
+          <CodeMirror
+            value="const a=1;"
+            theme={vscodeDark}
+            extensions={[javascript()]}
+            style={{ fontSize: 16 }}
+          />
+              </div>
+              <div className="text-white">test cases</div>
+      </Split>
+    </div>
+  );
 };
-
-const Playground:React.FC<PlaygroundProps> = () => {
-    
-    return <div>Have a good coding</div>
-}
 export default Playground;
